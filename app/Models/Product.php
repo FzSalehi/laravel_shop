8f1151699638ccc8deb2a77507f279c33aa06658
Faraz_Salehi <faraz67fz@gmail.com>
2021-11-01 14:42:58 +0330

    created method to give images url.

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,4 +8,33 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
+
+    public $guarded = [];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function demoUrl()
+    {
+        return 'storage/'.$this->demo_url;
+    }
+
+    public function thumbnailUrl()
+    {
+        return 'storage/'.$this->thumbnail_url;
+    }
+
+    public function sourceUrl()
+    {
+        return storage_path('app/private/'.$this->source_url);
+    }
+
+
 }
